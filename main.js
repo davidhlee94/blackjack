0baece0bfb9871d59d42feffd0278505c765d76f
davidhlee94 <davidhlee94@gmail.com>
2022-11-15 15:40:29 -0800

    created deck and added random selection feature in a different way

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
 //BlackJack Game
 
 //constants
-const dealer = document.getElementById('dealer')
-const player = document.getElementById('player')
-const hit = document.getElementById('hit-btn')
-const stay = document.getElementById('stay-btn')
-const reset = documnent.getElementById('reset-btn')
+// const dealer = document.getElementById('dealer')
+// const player = document.getElementById('player')
+// const hit = document.getElementById('hit-btn')
+// const stay = document.getElementById('stay-btn')
+// const reset = document.getElementById('reset-btn')
 
 //Card Suits and values
 
-const suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+const suit = ['♥', '♦', '♠', '♣']
 const value = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
 
 
-function randomCard(){
-    let randomSuit = suits[Math.floor(Math.random() * suits.length)];
-    let randomValue = value[Math.floor(Math.random() * value.length)];
-    let cardSelected = `${randomValue} of ${randomSuit}`;
-    console.log(cardSelected);
+let deck = [];
+function creatingDeck() {
+    for(let i = 0; i < suit.length; i++){
+        for(let j = 0; j < value.length; j++){
+        deck.push(value[j] + " of " + suit[i])
+        }
+    } console.log(deck)
 }
-console.log(randomCard())
+creatingDeck();
+
+console.log(deck[Math.floor(Math.random() * deck.length)])
+
+
+// function randomCard(){
+//     let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+//     let randomValue = value[Math.floor(Math.random() * value.length)];
+//     let cardSelected = `${randomValue} of ${randomSuit}`;
+//     console.log(cardSelected);
+// }
+// console.log(randomCard())
