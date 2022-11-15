2141918af9d5c2d6add97fc1362e87c8d31caaea
davidhlee94 <davidhlee94@gmail.com>
2022-11-15 14:22:47 -0800

    created obj on cards, created function that pulls random card

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 //BlackJack Game
 
 //Card Suits and values
-let suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
-let value = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
+const cards = {
+    suits: ['Diamonds', 'Hearts', 'Clubs', 'Spades'],
+    value: ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'],
+    usedCards: [],
+    
+}
 
 
+function randomCard(){
+    let randomSuit = cards.suits[Math.floor(Math.random() * cards.suits.length)];
+    let randomValue = cards.value[Math.floor(Math.random() * cards.value.length)];
+    let cardSelected = `${randomValue} of ${randomSuit}`;
+    console.log(cardSelected)
+}
+console.log(randomCard())
