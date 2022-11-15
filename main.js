bc89da1665de86d03618329a83d614f32ed844ae
davidhlee94 <davidhlee94@gmail.com>
2022-11-15 15:04:10 -0800

    added the constants

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 //BlackJack Game
 
+//constants
+const dealer = document.getElementById('dealer')
+const player = document.getElementById('player')
+const hit = document.getElementById('hit-btn')
+const stay = document.getElementById('stay-btn')
+const reset = documnent.getElementById('reset-btn')
+
 //Card Suits and values
-const cards = {
-    suits: ['Diamonds', 'Hearts', 'Clubs', 'Spades'],
-    value: ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'],
-    usedCards: [],
-    
-}
+
+const suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+const value = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
 
 
 function randomCard(){
-    let randomSuit = cards.suits[Math.floor(Math.random() * cards.suits.length)];
-    let randomValue = cards.value[Math.floor(Math.random() * cards.value.length)];
+    let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    let randomValue = value[Math.floor(Math.random() * value.length)];
     let cardSelected = `${randomValue} of ${randomSuit}`;
-    console.log(cardSelected)
+    console.log(cardSelected);
 }
 console.log(randomCard())
